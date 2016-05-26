3fa1ec790d83bd8815b89408b341f6ab6757232c
Mosab Sasi <smosab@gmail.com>
2016-05-26 10:41:02 -0600

    crypto sqaure spike

---
+++ crypto_square/crypto_square.rb
@@ -0,0 +1,27 @@
+Crypto square is a program that asks the user for a message (english text) to compose into a secret message called a square code. The program then outputs the encoded version of the text.
+
+input message
+
+encrypt
+
+squared code
+
+normalize
+
+rectangle
+
+columns
+
+rows
+
+characters
+
+class Crypto_Square
+
+  def ask_user_for_message(msg)
+
+  end
+
+
+
+end
