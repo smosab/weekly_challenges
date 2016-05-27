94bf16cb369b8f86eed158d1c9648db909fe43b6
Moe <smosab@gmail.com>
2016-05-27 17:59:28 +0000

    testing Crypto sqaure program

--- crypto_square/crypto_square.rb
+++ crypto_square/crypto_square.rb
@@ -31,6 +31,7 @@ class Message
   end
 
   def output
+    puts "Coded Message:"
     puts message
   end
 
@@ -39,6 +40,7 @@ class Message
       return Math.sqrt(message.size)
     else return Math.sqrt(message.size).ceil
     end
+    
   end
 
   def break_into_rows
@@ -48,10 +50,32 @@ class Message
       square_string << "#{message.slice!(0..columns)}\n"
     end
     @message = square_string
+    puts "Rectangled message: "
+    puts @message
+    puts ""
+  end
+  
+  
+  def code_message
+    coded_string = ''
+    
+    #split the msg
+    split_message = message.split("\n").map { |x| x.split("") }
+    
+    #iterate through each row (or word group) and place the first character of each word-group into a new string.
+    while split_message.flatten.any?
+      split_message.each do |wordgroup|
+        coded_string << wordgroup.shift unless wordgroup[0] == nil
+      end
+      coded_string << " "
+    end
+    
+    # assing the message variable the new coded string
+    @message = coded_string.rstrip
   end
 end
 
-class Crypto_Square
+class Crypto
   attr_accessor :message
 
   def initialize(msg)
@@ -59,10 +83,18 @@ class Crypto_Square
   end
 
   # ask_user_for_message(msg)
-  def encrypt
+  def normalize
     message.normalize
+  end
+  
+  def size
+    message.size_of_square
+  end
+  
+  def encrypt
+    normalize
     message.break_into_rows
-    # message.code_message
+    message.code_message
   end
 
   def output
@@ -71,12 +103,12 @@ class Crypto_Square
 
 end
 
-# secretmessage = Crypto_Square.new("Money can't buy you happiness, but it does bring you a more pleasant form of misery.")
-# secretmessage = Crypto_Square.new("Sometimes you win, sometimes you learn.")
-# secretmessage = Crypto_Square.new("Man who drop watch in toilet have shitty time.")
-# secretmessage = Crypto_Square.new("If man was meant to stay on the ground, god would have given us roots.")
-
-secretmessage = Crypto_Square.new("Hello")
+# secretmessage = Crypto.new("Money can't buy you happiness, but it does bring you a more pleasant form of misery.")
+# secretmessage = Crypto.new("Sometimes you win, sometimes you learn.")
+# secretmessage = Crypto.new("Man who drop watch in toilet have shitty time.")
+# secretmessage = Crypto.new("If man was meant to stay on the ground, god would have given us roots.")
+secretmessage = Crypto.new('Oh hey, this is nuts!')
+# secretmessage = Crypto.new("Hello")
 
 secretmessage.encrypt
 secretmessage.output
