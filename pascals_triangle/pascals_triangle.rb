ba6e9a8d9950e6bb0e9dd6c9faa0c4641f85a2f3
Moe <smosab@gmail.com>
2016-06-04 00:51:24 +0000

    pascal's tri

--- pascals_triangle/pascals_triangle.rb
+++ pascals_triangle/pascals_triangle.rb
@@ -1,42 +1,8 @@
 require 'pry'
 
-tri_array = [[1], [1,1]]
-temp_arr = []
-c = 0
-# tri_array.last.size.times do |c|
-  # binding.pry
-  tri_array.last.each do |num|
-    binding.pry
-    # add zero to the first index
-    temp_arr << num + c
-    # tri_array << [num, num + c]
-    # p tri_array.last[n] == nil ? tri_array.last[n-1] + 0 : tri_array.last[n] + 0
-    c += 1
-  end
-# end
-
-# class Triangle
-
-#   def initialize(number) #4
-#     tri_array = [[1]]
-#     loop_count = tri_array.size + 1
-
-#     tri_array.last do
-#       temp_arr = []
-
-#       loop_count.size.times do |n|
-#         n == 0 ? temp_arr.push 1
-#         n == tri_array.find_index(tri_array.last) ? temp_arr.push 1
-
-#     end
-#   end
-# end
 
 
 
-# Triangle.new(2)
-
-# tri_array = [[1], [1,1], [1,2,1]]
 
 # 1. First array is [1]
 # 2. each subsequent number do the following to the previous array to create a new one:
